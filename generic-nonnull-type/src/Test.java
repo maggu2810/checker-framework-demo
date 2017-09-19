0ccc14b0e37e68105ec4246a87327e31d1d2f4e1
Markus Rathgeb <maggu2810@gmail.com>
2017-09-19 15:48:09 +0200

    add test for generic nonnull type
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- generic-nonnull-type/src/Test.java
+++ generic-nonnull-type/src/Test.java
@@ -1,5 +1,34 @@
 
-import org.eclipse.smarthome.automation.handler.BaseModuleHandlerFactory;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
-public class Test extends BaseModuleHandlerFactory {
+public class Test {
+
+    public interface Consumer {
+
+        <@NonNull T> void foo(final Container<@NonNull T> c);
+    }
+
+    public static class Container<@NonNull T> {
+
+        private @NonNull
+        T t;
+
+        public Container(final @NonNull T t) {
+            this.t = t;
+        }
+
+        public void set(final @NonNull T t) {
+            this.t = t;
+        }
+
+        public @NonNull
+        T get() {
+            return t;
+        }
+
+        public void call(final @NonNull Consumer c) {
+            c.foo(this);
+        }
+    }
+
 }
